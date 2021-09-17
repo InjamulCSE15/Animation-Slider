4bacefac6ee774061324cf67e6854fb28c5f0e5f
unknown <yeasin.mohammod018@gmail.com>
2021-09-18 03:05:22 +0600

    project done

---
+++ myScript.js
@@ -0,0 +1,12 @@
+// console.log('My JavaScript file is working...');
+
+let count = 1;
+
+setInterval(function() {
+    document.getElementById('radio' + count).checked = true;
+    count++;
+
+    if (count > 4) {
+        count = 1;
+    }
+},5000);
